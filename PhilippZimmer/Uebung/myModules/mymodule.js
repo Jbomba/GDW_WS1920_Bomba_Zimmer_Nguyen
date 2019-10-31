322b22463a70594b493855ab0af74fadbdc3944b
PhilippZimmer <philipp-a-zimmer@web.de>
2019-10-31 18:35:56 +0100

    AB3 Nr2 abschluss Donnerstag

--- PhilippZimmer/Uebung/myModules/mymodule.js
+++ PhilippZimmer/Uebung/myModules/mymodule.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 
-const readJSON = ( path, callback) => {
+/*const readJSON = ( path, callback) => {
     fs.readFile(path,'utf8', callback);
-};
+}*/
 
-var test = readJSON.toString;
-test
+fs.readFile( './../cities.json',callback)
 
-module.exports = {
+function stadtFinden ( suche ) {
+}
+
+function stadtLoeschen ( suche ) {
+
+}
+
+function stadtHinzuf ( einfuegen ) {
+
+}
+
+/*module.exports = {
     readJSON
-};
+};*/
