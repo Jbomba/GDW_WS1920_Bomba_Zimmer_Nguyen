7a8e2827460db1a1f8f79027f3763398394045c2
PhilippZimmer <philipp-a-zimmer@web.de>
2019-11-05 10:54:56 +0100

    AB3 Zwischenstand

--- PhilippZimmer/Uebung/ab3.js
+++ PhilippZimmer/Uebung/ab3.js
@@ -1,18 +1,37 @@
 const JSONReader = require ('./myModules/mymodule.js');
+const readline = require('readline');
+const eingabe = readline.createInterface({input: process.stdin, output: process.stdout});
 
 JSONReader.readJSON('./cities.json', (err,result)=> {
     if (err) throw err
     try{
         let stadt = JSON.parse(result);
-        console.log(stadt);
+        eingabe.question( '1 = Stadt finden und loeschen, 2 = Stadt Hinzufuegen: ', function (antwort){
+        if (antwort == 1){
+            eingabe.question( 'Welche Stadt wollen Sie hinzufuegen: ', function( antwhinz){
+                eingabe.close();
+                JSONReader.stadtFinden ( antwhinz, stadt );
+            })
+        }
+        else if (antwort == 2) {
+            console.log('hinzufuegen');
+            eingabe.question( 'Name der Stadt: ', function( antwName) {
+                eingabe.question( 'Bundesland: ', function( antwBundesland){
+                    eingabe.question( 'Einwohnerzahl: ', function( antwEinw){
+                        eingabe.close();
+                        JSONReader.stadtHinzuf ( antwEinw, antwName, antwBundesland, stadt);
+                    })
+                })
+            }) 
+        }
+        else {
+            console.log('Falsche Eingabe');
+            process.exit();
+        }
+        })
     } catch (error) {
         console.log(error);
         process.exit();
     }
 })
 
-/*JSONReader.stadtFinden
-JSONReader.stadtHinzuf
-JSONReader.stadtLoeschen
-JSONReader.writeJSON
-*/
--- PhilippZimmer/Uebung/myModules/mymodule.js
+++ PhilippZimmer/Uebung/myModules/mymodule.js
@@ -8,23 +8,27 @@ const writeJSON = ( path, callback ) => {
     fs.writeFile ( path, 'utf8', callback);
 }
 
-module.exports = {
-    readJSON, writeJSON //stadtFinden, stadtLoeschen, stadtHinzuf
-};
+const stadtFinden = ( suche, array ) => {
 
-const stadtFinden = ( suche ) => {
-    const stadt = [];
 }
 
 const stadtLoeschen = ( loeschen ) => {
 
 }
 
-const stadtHinzuf = ( einfuegen ) => {
-    const stadt = [{}];
-    //stadt.push
-
-
+const stadtHinzuf = ( zahl, stadt, bundesl, array ) => {
+    let neuStadt = { einwohner : zahl,
+                     name : stadt,
+                     bundesland : bundesl
+    };
+    array.push( neuStadt );
+    for(let i=0;i<array.length;i++){
+        console.log(array[i]);
+    }
 }
+
+module.exports = {
+    readJSON, writeJSON, stadtHinzuf, stadtFinden
+};
 
 
