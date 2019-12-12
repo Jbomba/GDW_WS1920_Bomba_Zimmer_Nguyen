39f66e9173a2a751414f6a9cae25141e85a44435
PhilippZimmer <philipp-a-zimmer@web.de>
2019-12-12 15:11:26 +0100

    API ende

--- PhilippZimmer/API_ajax/weatherunlocked.js
+++ PhilippZimmer/API_ajax/weatherunlocked.js
@@ -7,13 +7,13 @@
 //deutsche Sprache
 //http://api.weatherunlocked.com/api/current/de.51643?lang=de&app_id={dcdfab73}&app_key={f7cdadd161bb7a81c9fd7dbcba62418f}
 
-
+/*
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
 <script>
 
     $.ajax({
-        url: "http://api.weatherunlocked.com/api/current/de.51643?lang=de&app_id={dcdfab73}&app_key={f7cdadd161bb7a81c9fd7dbcba62418f}",
+        url: "http://api.weatherunlocked.com/api/current/51.50,-0.12?lang=de&app_id={dcdfab73}&app_key={f7cdadd161bb7a81c9fd7dbcba62418f}",
         type: "GET",
         success: function (parsedResponse, statusText, jqXhr) {
 
@@ -27,3 +27,25 @@
     });
 
 </script>
+*/
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+  (function() {
+    var fetchWeatherData = function(51.50, -0.12) {
+      // You need to sign up to Weather Unlocked for an account. Once you do,
+      // you will find your APP ID and APP KEY in your account dashboard.
+      var wuAppId = 'dcdfab73';
+      var wuAppKey = 'f7cdadd161bb7a81c9fd7dbcba62418f';
+      jQuery.getJSON('https://api.weatherunlocked.com/api/current/' + latitude + ',' + longitude + '?app_id=' + wuAppId + '&app_key=' + wuAppKey)
+        .done(function(data) {
+          window.dataLayer.push({
+            event: 'weatherDone',
+            weather: data.wx_desc,
+            temperature: data.temp_c
+          });
+        }).fail(function(jq, status, msg) {
+          console.log('Weather request failed: ' + status + ' - ' + msg);
+        });
+    };
+    </script>
