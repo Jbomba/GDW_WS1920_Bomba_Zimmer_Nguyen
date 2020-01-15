2e4594edc9b52e04a2969903a482846aa766f4d3
PhilippZimmer <philipp-a-zimmer@web.de>
2020-01-15 18:34:08 +0100

    json (hinweise) entfernt, js json read, pars

--- DummyDaten/Test.js
+++ DummyDaten/Test.js
@@ -1,12 +1,37 @@
 const test = "Test";
 console.log(test);
 
-"use strict";
+const fs = require('fs');
+var content;
 
-fetch("./Arbeitsplan.json")
-    .then(function(resp){
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+
+
+fs.readFile('./DummyDaten/Arbeitsplan', function read(err,data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+    const obj = JSON.parse(content);
+console.log(obj);
+console.log();
+
+const arrayTag1 = obj[0];
+console.log(arrayTag1);
+console.log();
+
+const nichtanwesendTag1 = arrayTag1[0];
+console.log(nichtanwesendTag1);
+console.log();
+
+const person1NichtanwesendTag1 = nichtanwesendTag1[0];
+console.log(person1NichtanwesendTag1.name);
+
+})
+
+
+
+
+
+
+
+
